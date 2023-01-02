30e7b8297b6628c00a41f434eb37e3527d8ecf6d
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-02 13:52:34 +0100

    added some simple responsivness for start screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,26 +9,26 @@ const Home: NextPage = () => {
 
   return (
     <div>
-      <div className="relative w-full py-20 px-10 text-center md:py-32">
-        <h1 className="mb-5 text-6xl font-black md:mb-8 md:text-8xl">
+      <div className="relative w-full px-2 pt-8 text-center sm:py-20 md:px-10 md:py-32">
+        <h1 className="mb-5 text-4xl font-black md:mb-8 md:text-8xl">
           Techover
           <br />
           Battleground
         </h1>
-        <p className="mb-5 text-text-dimmed">
+        <p className="mb-5 text-text-dimmed max-sm:text-sm">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam,
           laudantium?
         </p>
         {sessionData ? (
-          <div className="flex justify-center gap-3">
+          <div className="flex flex-col justify-center gap-3 sm:flex-row">
             <Link
-              className="rounded-full bg-bg-dimmed px-10 py-3 font-semibold text-white no-underline transition hover:bg-bg-dimmed/60"
+              className="rounded-full bg-bg-dimmed px-10 py-3 font-semibold text-white no-underline transition hover:bg-bg-dimmed/60 max-md:text-sm"
               href="/problems"
             >
               Start solving problems
             </Link>
             <button
-              className="rounded-full bg-bg-dimmed px-10 py-3 font-semibold text-white no-underline transition hover:bg-bg-dimmed/60"
+              className="rounded-full bg-bg-dimmed px-10 py-3 font-semibold text-white no-underline transition hover:bg-bg-dimmed/60 max-md:text-sm"
               onClick={() => signOut()}
             >
               Sign out
