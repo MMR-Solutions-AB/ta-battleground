b0c6bb9aabf978c1559f1e47f391669b7cff0bad
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-02 10:52:30 +0100

    added unique items

---
+++ src/data/problems/unique-items/data.ts
@@ -0,0 +1,26 @@
+import type { Problem } from "../../Problem";
+
+export const data: Problem<number[], number[]> = {
+  name: "Unique items",
+  difficulty: "medium",
+  number: 12,
+  arguments: ["a"],
+  testCases: [
+    {
+      input: [[4, 5, 1, 3, 5, 1]],
+      output: [1, 3, 4, 5],
+    },
+    {
+      input: [[2, 0, 0, 3]],
+      output: [0, 2, 3],
+    },
+    {
+      input: [[4, 4, 4]],
+      output: [4],
+    },
+    {
+      input: [[12, 18, 25]],
+      output: [12, 18, 25],
+    },
+  ],
+};
---
+++ src/data/problems/unique-items/description.md
@@ -0,0 +1,21 @@
+# Unique items
+
+Skriv en funktion **unique(a)** som returnerar en lista med de unika elementen i listan **a**. Listan ska vara sorterad i stigande ordning.
+
+## Exempel 1
+
+**_Input_**
+
+```js
+a = [4, 5, 1, 3, 5, 1];
+```
+
+**_Output_**
+
+```bash
+[1, 3, 4, 5]
+```
+
+**_Förklaring_**
+
+I listan **[4, 5, 1, 3, 5, 1]** så är det enda unika värdena **[4, 5, 1, 3]**, om vi sorterar vår nya array får vi **[1, 3, 4, 5]**
