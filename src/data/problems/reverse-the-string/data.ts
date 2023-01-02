f0ccae171b0ea56103fbded11d13d24dccadb740
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-02 10:57:29 +0100

    added reverse-the-string

---
+++ src/data/problems/reverse-the-string/data.ts
@@ -0,0 +1,26 @@
+import type { Problem } from "../../Problem";
+
+export const data: Problem<string, string> = {
+  name: "Reverse the string",
+  difficulty: "easy",
+  number: 13,
+  arguments: ["s"],
+  testCases: [
+    {
+      input: [`"techover"`],
+      output: "revohcet",
+    },
+    {
+      input: [`"hello"`],
+      output: "olleh",
+    },
+    {
+      input: [`"december"`],
+      output: "rebmeced",
+    },
+    {
+      input: [`"racecar"`],
+      output: "racecar",
+    },
+  ],
+};
---
+++ src/data/problems/reverse-the-string/description.md
@@ -0,0 +1,17 @@
+# Reverse the string
+
+Skriv en funktion **reverse(s)** som returnerar en sträng som är omvänd versionen av strängen **s**.
+
+## Exempel 1
+
+**_Input_**
+
+```js
+s = "techover";
+```
+
+**_Output_**
+
+```bash
+revohcet
+```
