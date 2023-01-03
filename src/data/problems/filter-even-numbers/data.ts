edfbde71d66992a9a2bf1819632c81e1a97c8a65
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-03 22:02:43 +0100

    added filter even numbers

---
+++ src/data/problems/filter-even-numbers/data.ts
@@ -0,0 +1,23 @@
+import type { Problem } from "../../Problem";
+
+export const data: Problem<number[], number[]> = {
+  name: "Filter even numbers",
+  difficulty: "easy",
+  number: 32,
+  arguments: ["numbers"],
+  tags: ["basics", "numbers", "arrays"],
+  testCases: [
+    {
+      input: [[1, 2, 3, 4]],
+      output: [2, 4],
+    },
+    {
+      input: [[2, 5]],
+      output: [2],
+    },
+    {
+      input: [[3, 5, 6, 8, 10]],
+      output: [6, 8, 10],
+    },
+  ],
+};
---
+++ src/data/problems/filter-even-numbers/description.md
@@ -0,0 +1,17 @@
+# Filter even numbers
+
+Skriv en funktion **evenNumbers(numbers)** som tar emot en array av heltal och returnerar en array med bara de jämna heltalen.
+
+## Exempel 1
+
+**_Input_**
+
+```bash
+numbers = [1, 2, 3, 4]
+```
+
+**_Output_**
+
+```bash
+[2, 4]
+```
