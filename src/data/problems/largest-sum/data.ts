5d2175f7350bf887abcd1a76012e1e4b2e0f02af
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-03 18:59:00 +0100

    added Largest sum

---
+++ src/data/problems/largest-sum/data.ts
@@ -0,0 +1,27 @@
+import type { Problem } from "../../Problem";
+
+export const data: Problem<number[], number> = {
+  name: "Largest sum",
+  difficulty: "medium",
+  number: 28,
+  arguments: ["arr"],
+  tags: ["numbers", "arrays"],
+  testCases: [
+    {
+      input: [[-2, 1, -3, 4, -1, 2, 1, -5, 4]],
+      output: 6,
+    },
+    {
+      input: [[1, 2, 3, -8, 10]],
+      output: 10,
+    },
+    {
+      input: [[4, 8, 9, -5, 1]],
+      output: 21,
+    },
+    {
+      input: [[-4, -7, 6, 7, -8, 3]],
+      output: 13,
+    },
+  ],
+};
---
+++ src/data/problems/largest-sum/description.md
@@ -0,0 +1,39 @@
+# Largest sum
+
+Skriv en funktion **largestSum(arr)** som hittar den största summan av kontinuerliga element i en lista.
+
+## Exempel 1
+
+**_Input_**
+
+```bash
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+```
+
+**_Output_**
+
+```bash
+6
+```
+
+**_Förklaring_**
+
+Den största summan får vi genom att addera nummerna **4, -1, 2, 1**
+
+## Exempel 2
+
+**_Input_**
+
+```bash
+arr = [1, 2, 3, -8, 10]
+```
+
+**_Output_**
+
+```bash
+10
+```
+
+**_Förklaring_**
+
+Den största summan får vi genom att enbart ta **10**
