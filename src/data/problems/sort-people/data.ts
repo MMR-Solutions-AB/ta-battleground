4ce0ecdf3c82bd7a63b1fa458345c69c7c763c34
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-03 23:02:56 +0100

    added sort people

---
+++ src/data/problems/sort-people/data.ts
@@ -0,0 +1,56 @@
+import type { Problem } from "../../Problem";
+
+export const data: Problem<
+  { name: string; age: number }[],
+  { name: string; age: number }[]
+> = {
+  name: "Sort people",
+  difficulty: "easy",
+  number: 34,
+  arguments: ["people"],
+  tags: ["basics", "arrays", "objects"],
+  testCases: [
+    {
+      input: [
+        [
+          { name: "Filip", age: 28 },
+          { name: "Matheus", age: 19 },
+          { name: "Mr.Heavenstone", age: 21 },
+        ],
+      ],
+      output: [
+        { name: "Matheus", age: 19 },
+        { name: "Mr.Heavenstone", age: 21 },
+        { name: "Filip", age: 28 },
+      ],
+    },
+    {
+      input: [
+        [
+          { name: "Bob", age: 25 },
+          { name: "Alice", age: 20 },
+          { name: "Charlie", age: 30 },
+        ],
+      ],
+      output: [
+        { name: "Alice", age: 20 },
+        { name: "Bob", age: 25 },
+        { name: "Charlie", age: 30 },
+      ],
+    },
+    {
+      input: [
+        [
+          { name: "Ronaldo", age: 37 },
+          { name: "Messi", age: 35 },
+          { name: "Neymar", age: 33 },
+        ],
+      ],
+      output: [
+        { name: "Neymar", age: 33 },
+        { name: "Messi", age: 35 },
+        { name: "Ronaldo", age: 37 },
+      ],
+    },
+  ],
+};
---
+++ src/data/problems/sort-people/description.md
@@ -0,0 +1,47 @@
+# Sort people
+
+Skriv en funktion **sortPeople(people)** som tar emot en array av personer som objekt, där varje person har två keys, **name** och **age**. Funktionen ska returnera en array av personerna sorterade efter ålder
+
+## Exempel 1
+
+**_Input_**
+
+```bash
+people = [
+  { name: "Filip", age: 28 },
+  { name: "Matheus", age: 19 },
+  { name: "Mr.Heavenstone", age: 21 },
+]
+```
+
+**_Output_**
+
+```js
+[
+  { name: "Matheus", age: 19 },
+  { name: "Mr.Heavenstone", age: 21 },
+  { name: "Filip", age: 28 },
+];
+```
+
+## Exempel 2
+
+**_Input_**
+
+```js
+people = [
+  { name: "Bob", age: 25 },
+  { name: "Alice", age: 20 },
+  { name: "Charlie", age: 30 },
+];
+```
+
+**_Output_**
+
+```js
+[
+  { name: "Alice", age: 20 },
+  { name: "Bob", age: 25 },
+  { name: "Charlie", age: 30 },
+];
+```
