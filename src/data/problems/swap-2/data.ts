9b823f6d7dc460c96acc14e0446e96ca86e6c5a6
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-03 16:39:24 +0100

    added swap 2

---
+++ src/data/problems/swap-2/data.ts
@@ -0,0 +1,27 @@
+import type { Problem } from "../../Problem";
+
+export const data: Problem<string, string> = {
+  name: "Swap 2",
+  difficulty: "medium",
+  number: 23,
+  arguments: ["s"],
+  tags: ["strings"],
+  testCases: [
+    {
+      input: [`"abcd"`],
+      output: "badc",
+    },
+    {
+      input: [`"mendes"`],
+      output: "emdnse",
+    },
+    {
+      input: [`"gunillas"`],
+      output: "uginllsa",
+    },
+    {
+      input: [`"xy"`],
+      output: "yx",
+    },
+  ],
+};
---
+++ src/data/problems/swap-2/description.md
@@ -0,0 +1,35 @@
+# Swap 2
+
+Skriv en funktion **swap2(s)** som byter plats på varannan bokstav i en sträng.
+
+**_Förklaring_**
+
+En förklaring
+
+## Exempel 1
+
+**_Input_**
+
+```bash
+s = "abcd"
+```
+
+**_Output_**
+
+```bash
+"badc"
+```
+
+## Exempel 2
+
+**_Input_**
+
+```bash
+s = "mendes"
+```
+
+**_Output_**
+
+```bash
+"emdnse"
+```
