8eb6e029d510ec209a4f3a35f9dece30bfbeaaf5
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-04 00:26:09 +0100

    added reverse the integer

---
+++ src/data/problems/reverse-the-integer/data.ts
@@ -0,0 +1,27 @@
+import type { Problem } from "../../Problem";
+
+export const data: Problem<number, number> = {
+  name: "Reverse the integer",
+  difficulty: "medium",
+  number: 40,
+  arguments: ["s"],
+  tags: ["numbers"],
+  testCases: [
+    {
+      input: [123],
+      output: 321,
+    },
+    {
+      input: [-12],
+      output: -21,
+    },
+    {
+      input: [81],
+      output: 18,
+    },
+    {
+      input: [-40892],
+      output: -29802,
+    },
+  ],
+};
---
+++ src/data/problems/reverse-the-integer/description.md
@@ -0,0 +1,31 @@
+# Reverse the integer
+
+Skriv en funktion **reverseTheInteger(n)** som tar emot ett heltal och returnerar talet i omvänd ordning. Ifall **n** är negativt ska det som returneras också vara negativt
+
+## Exempel 1
+
+**_Input_**
+
+```bash
+n = 123
+```
+
+**_Output_**
+
+```bash
+321
+```
+
+## Exempel 2
+
+**_Input_**
+
+```bash
+n = -12
+```
+
+**_Output_**
+
+```bash
+-21
+```
