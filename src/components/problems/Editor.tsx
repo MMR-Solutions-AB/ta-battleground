609b1cafc8f4c514100a6c077027622e30d4ab03
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-07 16:00:14 +0100

    🪄 added format code button

--- src/components/problems/Editor.tsx
+++ src/components/problems/Editor.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import TestCases from "./TestCases";
 import { generateStarterCode } from "@/utils/generateStarterCode";
-import { Code, Settings, RefreshCw } from "react-feather";
+import { Code, PenTool, Settings, RefreshCw } from "react-feather";
 import { minify } from "terser";
 import { useEditorSettings, allThemes } from "@/context/EditorContext";
 import { useModal } from "@/context/ModalContext";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { EditorView } from "codemirror";
+import { js_beautify } from "js-beautify";
 
 interface EditorProps {
   starterCode?: string;
@@ -33,7 +34,7 @@ const Editor: React.FC<EditorProps> = ({
     <div className="grid grid-rows-[auto_1fr] overflow-y-scroll">
       <div className="flex flex-shrink-0 flex-wrap items-center justify-between bg-bg-dark pb-2 pr-2 pt-2 text-sm text-text-dimmed">
         <span>{`{ ${code.length} }`}</span>
-        <div className="flex gap-2">
+        <div className="flex flex-wrap gap-2">
           <button
             onClick={async () => {
               setCode(generateStarterCode(problemName, problemArgs));
@@ -42,6 +43,21 @@ const Editor: React.FC<EditorProps> = ({
           >
             <RefreshCw className="h-3 w-3" />
             Reset code
+          </button>
+          <button
+            onClick={async () => {
+              try {
+                setCode(js_beautify(code));
+              } catch (error) {
+                alert(
+                  "Kunde inte formatera din kod eftersom att du har syntax errors, fixa dem och försök igen"
+                );
+              }
+            }}
+            className="flex items-center gap-2 rounded-md bg-bg-dimmed py-1 px-3"
+          >
+            <PenTool className="h-3 w-3" />
+            Format code
           </button>
           <button
             onClick={async () => {
@@ -70,7 +86,7 @@ const Editor: React.FC<EditorProps> = ({
       </div>
 
       <div className="w-full overflow-y-scroll">
-        <div className="grid h-[calc(100vh-200px)] w-full flex-1 flex-shrink-0 overflow-y-scroll">
+        <div className="grid h-[calc(100vh-176px)] w-full flex-1 flex-shrink-0 overflow-y-scroll">
           <CodeMirror
             value={code}
             onChange={setCode}
