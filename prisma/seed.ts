9f4a1beec3180712f36c3284dffc982ae568280a
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-08 23:24:10 +0100

    improved prisma seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,46 +4,56 @@ import { getAllProblems } from "../src/data/getAllProblems";
 async function main() {
   const problems = await getAllProblems();
 
+  let promises: Promise<any>[] = [];
+
   for (let i = 0; i < problems.length; i++) {
     const problem = problems[i];
     if (!problem) continue;
+    console.log("at : ", i);
 
-    await prisma.problem.upsert({
-      where: { number: problem.number },
-      update: {
-        description: problem.description,
-        name: problem.name,
-        testCases: problem.testCases,
-        arguments: problem.arguments,
-        number: problem.number,
-        difficulty: problem.difficulty,
-        tags: {
-          set: [],
-          connectOrCreate: problem.tags.map((tag) => ({
-            where: { name: tag },
-            create: {
-              name: tag,
-            },
-          })),
+    if (i % 10 === 0) {
+      await Promise.all(promises);
+      promises = [];
+    }
+
+    promises.push(
+      prisma.problem.upsert({
+        where: { number: problem.number },
+        update: {
+          description: problem.description,
+          name: problem.name,
+          testCases: problem.testCases,
+          arguments: problem.arguments,
+          number: problem.number,
+          difficulty: problem.difficulty,
+          tags: {
+            set: [],
+            connectOrCreate: problem.tags.map((tag) => ({
+              where: { name: tag },
+              create: {
+                name: tag,
+              },
+            })),
+          },
         },
-      },
-      create: {
-        description: problem.description,
-        name: problem.name,
-        arguments: problem.arguments,
-        testCases: problem.testCases,
-        number: problem.number,
-        difficulty: problem.difficulty,
-        tags: {
-          connectOrCreate: problem.tags.map((tag) => ({
-            where: { name: tag },
-            create: {
-              name: tag,
-            },
-          })),
+        create: {
+          description: problem.description,
+          name: problem.name,
+          arguments: problem.arguments,
+          testCases: problem.testCases,
+          number: problem.number,
+          difficulty: problem.difficulty,
+          tags: {
+            connectOrCreate: problem.tags.map((tag) => ({
+              where: { name: tag },
+              create: {
+                name: tag,
+              },
+            })),
+          },
         },
-      },
-    });
+      })
+    );
   }
 }
 
