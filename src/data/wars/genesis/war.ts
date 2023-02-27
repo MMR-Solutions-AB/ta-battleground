37ef793cc59252a14b8d8e668e7d78f2ff1ee8b1
imMatheus <matheusmendesbarata03@gmail.com>
2023-02-27 18:28:20 +0100

    changed war dates

--- src/data/wars/genesis/war.ts
+++ src/data/wars/genesis/war.ts
@@ -3,6 +3,6 @@ import type { WarCreate } from "../../War";
 export const data: WarCreate = {
   name: "Genesis",
   number: 1,
-  startTime: new Date("25 feb 2023"),
-  endTime: new Date("2 mars 2023"),
+  startTime: new Date("5 mars 2023"),
+  endTime: new Date("12 mars 2023"),
 };
--- src/data/wars/the-trident/war.ts
+++ src/data/wars/the-trident/war.ts
@@ -3,6 +3,6 @@ import type { WarCreate } from "../../War";
 export const data: WarCreate = {
   name: "The trident",
   number: 3,
-  startTime: new Date("11 mars 2023"),
-  endTime: new Date("18 mars 2023"),
+  startTime: new Date("21 mars 2023"),
+  endTime: new Date("28 mars 2023"),
 };
