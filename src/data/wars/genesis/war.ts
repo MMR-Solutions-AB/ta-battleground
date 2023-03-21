87db9e770367712402b792cb4b49694db87cee92
imMatheus <matheusmendesbarata03@gmail.com>
2023-03-21 13:09:40 +0100

    changed start time for wars

--- src/data/wars/genesis/war.ts
+++ src/data/wars/genesis/war.ts
@@ -3,6 +3,6 @@ import type { WarCreate } from "../../War";
 export const data: WarCreate = {
   name: "Genesis",
   number: 1,
-  startTime: new Date("26 mars 2023"),
-  endTime: new Date("27 mars 2023"),
+  startTime: new Date("26 mars 2024"),
+  endTime: new Date("27 mars 2024"),
 };
