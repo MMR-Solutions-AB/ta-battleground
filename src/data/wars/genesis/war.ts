91740316cd83b9f6ae989e5836b1c8651ab1b5c3
imMatheus <matheusmendesbarata03@gmail.com>
2023-03-29 18:28:39 +0200

    changed back start time

--- src/data/wars/genesis/war.ts
+++ src/data/wars/genesis/war.ts
@@ -3,6 +3,6 @@ import type { WarCreate } from "../../War";
 export const data: WarCreate = {
   name: "Genesis",
   number: 1,
-  startTime: new Date("29 mars 2023 18:05"),
+  startTime: new Date("30 mars 2023 17:45"),
   endTime: new Date("30 april 2023 17:45"),
 };
