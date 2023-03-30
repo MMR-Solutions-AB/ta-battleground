903b2dc9398b18a2ec380ecc38841ecd8a2b3c17
imMatheus <matheusmendesbarata03@gmail.com>
2023-03-30 18:11:58 +0200

    changed faction banners

--- prisma/runners/syncDBWithFactions.ts
+++ prisma/runners/syncDBWithFactions.ts
@@ -1,14 +1,15 @@
 import { prisma } from "../../src/server/db/client";
+import { type Prisma } from "@prisma/client";
 
 // Just add new object to the baseFaction variable if you would like to add new faction
 export async function syncDBWithFactions() {
   const factions = await prisma.faction.findMany();
 
-  const baseFactions = [
-    { name: "Typescript Titans" },
-    { name: "CSS Challengers" },
-    { name: "React Raiders" },
-    { name: "Node Ninjas" },
+  const baseFactions: Prisma.FactionCreateInput[] = [
+    { name: "Typescript Titans", image: "banner-1.svg" },
+    { name: "CSS Challengers", image: "banner-2.svg" },
+    { name: "React Raiders", image: "banner-3.svg" },
+    { name: "Node Ninjas", image: "banner-4.svg" },
   ];
 
   if (factions.length < baseFactions.length) {
