f15f484ee6d5c88595eb6437ad82b29938b495d4
imMatheus <matheusmendesbarata03@gmail.com>
2023-04-16 14:29:42 +0200

    removed Promise.all for sync war function

--- prisma/runners/syncDBWithWars.ts
+++ prisma/runners/syncDBWithWars.ts
@@ -7,7 +7,6 @@ import { getAllWars } from "../../src/data/getAllWars";
 // If new fields where to be added this file would need to be updated to support that
 export async function syncDBWithWars() {
   const wars = await getAllWars();
-  let warPromises: Promise<any>[] = [];
 
   const factions = await prisma.faction.findMany();
 
@@ -15,65 +14,34 @@ export async function syncDBWithWars() {
     const war = wars[i];
     if (!war) continue;
 
-    if (i % 10 === 0) {
-      await Promise.all(warPromises);
-      warPromises = [];
-    }
-
-    warPromises.push(
-      prisma.war.upsert({
-        where: { number: war.number },
-        update: {
-          name: war.name,
-          number: war.number,
-          startTime: war.startTime,
-          endTime: war.endTime,
-          problems: {
-            upsert: war.problems.map((problem) => ({
-              where: { number: problem.number },
-              update: {
-                description: problem.description,
-                name: problem.name,
-                testCases: problem.testCases,
-                arguments: problem.arguments,
-                number: problem.number,
-                difficulty: problem.difficulty,
-                tags: {
-                  set: [],
-                  connectOrCreate: problem.tags.map((tag) => ({
-                    where: { name: tag },
-                    create: {
-                      name: tag,
-                    },
-                  })),
-                },
+    await prisma.war.upsert({
+      where: { number: war.number },
+      update: {
+        name: war.name,
+        number: war.number,
+        startTime: war.startTime,
+        endTime: war.endTime,
+        problems: {
+          upsert: war.problems.map((problem) => ({
+            where: { number: problem.number },
+            update: {
+              description: problem.description,
+              name: problem.name,
+              testCases: problem.testCases,
+              arguments: problem.arguments,
+              number: problem.number,
+              difficulty: problem.difficulty,
+              tags: {
+                set: [],
+                connectOrCreate: problem.tags.map((tag) => ({
+                  where: { name: tag },
+                  create: {
+                    name: tag,
+                  },
+                })),
               },
-              create: {
-                description: problem.description,
-                name: problem.name,
-                testCases: problem.testCases,
-                arguments: problem.arguments,
-                number: problem.number,
-                difficulty: problem.difficulty,
-                tags: {
-                  connectOrCreate: problem.tags.map((tag) => ({
-                    where: { name: tag },
-                    create: {
-                      name: tag,
-                    },
-                  })),
-                },
-              },
-            })),
-          },
-        },
-        create: {
-          name: war.name,
-          number: war.number,
-          startTime: war.startTime,
-          endTime: war.endTime,
-          problems: {
-            create: war.problems.map((problem) => ({
+            },
+            create: {
               description: problem.description,
               name: problem.name,
               testCases: problem.testCases,
@@ -88,17 +56,39 @@ export async function syncDBWithWars() {
                   },
                 })),
               },
-            })),
-          },
-          contenders: {
-            createMany: {
-              data: factions.map((faction) => ({ factionId: faction.id })),
             },
+          })),
+        },
+      },
+      create: {
+        name: war.name,
+        number: war.number,
+        startTime: war.startTime,
+        endTime: war.endTime,
+        problems: {
+          create: war.problems.map((problem) => ({
+            description: problem.description,
+            name: problem.name,
+            testCases: problem.testCases,
+            arguments: problem.arguments,
+            number: problem.number,
+            difficulty: problem.difficulty,
+            tags: {
+              connectOrCreate: problem.tags.map((tag) => ({
+                where: { name: tag },
+                create: {
+                  name: tag,
+                },
+              })),
+            },
+          })),
+        },
+        contenders: {
+          createMany: {
+            data: factions.map((faction) => ({ factionId: faction.id })),
           },
         },
-      })
-    );
+      },
+    });
   }
-
-  await Promise.all(warPromises);
 }
