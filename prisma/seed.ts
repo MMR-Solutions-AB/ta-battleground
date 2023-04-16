6459d6b859c99ae44535893daa06824b419abb8c
imMatheus <matheusmendesbarata03@gmail.com>
2023-04-16 14:10:06 +0200

    commented out seed.ts file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,20 +7,16 @@ import { syncDBWithWarScores } from "./runners/syncDBWithWarScores";
 
 async function main() {
   // make sure all factions are there
-  await syncDBWithFactions();
-
+  // await syncDBWithFactions();
   // add and update all wars
-  await syncDBWithWars();
-
+  // await syncDBWithWars();
   // add and update all problems
-  await syncDBWithProblems();
-
+  // await syncDBWithProblems();
   // sync db with correct war scores
-  await syncDBWithWarScores();
-
+  // await syncDBWithWarScores();
   // make sure all scores are up to date
   // this does not to be used unless user table gets out of sync
-  await syncDBWithUserScores();
+  // await syncDBWithUserScores();
 }
 
 main()
